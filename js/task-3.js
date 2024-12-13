92afc30f3f08393a08ec86e9c0f921d1dcd8367b
Vlad Tsokota <v.tsokota.69@gmail.com>
2024-12-13 16:34:29 +0200

    commit message

--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,23 @@
-function filterArray(numbers, value) {
-    let array = [];
+const profile = {
+    username: "Jacob",
+    playTime: 300,
 
-    for (let i = 0; i < numbers.length; i++) {
-        if (numbers[i] > value) {
-            array.push(numbers[i]);
-        }
-    }
+    changeUsername(newName) {
+        profile.username = newName;
+    },
+    updatePlayTime(hours) {
+        profile.playTime = this.playTime + hours;
+    },
 
-    return array;
-}
+    getInfo() {
+        return `${this.username} has ${this.playTime} active hours!`;
+    },
+};
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
